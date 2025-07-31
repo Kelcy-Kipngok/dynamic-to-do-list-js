89cfad6626db78e5fe46d633cf34010de83d72e5
Kelcy <kelcykipngok@gmail.com>
2025-07-31 08:27:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     const addButton = document.getElementById('add-task-btn');
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
+
     function addTask() {
         const taskText = taskInput.value.trim();
 
@@ -22,8 +22,6 @@ document.addEventListener('DOMContentLoaded', function() {
             taskList.appendChild(li);
 
             taskInput.value = "";
-        } else {
-            alert("Please enter a task.");
         }
     }
 
